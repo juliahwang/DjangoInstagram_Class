d33dee8a6a2020e2d4f91f824d89416629b0ea20
JuliaHwang <qufskan9396@gmail.com>
2017-06-13 11:18:25 +0900

    add post_detail url, view, and template

--- django_app/post/urls.py
+++ django_app/post/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+
+    # post_detail과 매칭 ex_ /post/3/$, /post/345/$
+    # 숫자 1개 이상을 변수로 전달하기 위해 그룹화하고, 그룹명을 post_pk로 지정해준다.
+    # 정규표현식에서 매칭된 그룹을 키워드인수로 반환하는 방법 : 그룹 앞부분에 ?P<이름> 지정
+    # 일치하는 패턴을 가진 url을 찾을 경우 views로 가서 post_detail을 실행한다.
     url(r'^(?P<post_pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^create/$', views.post_create, name='post_create'),
     url(r'^(?P<post_pk>\d+)/modify/$', views.post_modify, name='post_modify')
--- django_app/post/views.py
+++ django_app/post/views.py
@@ -1,8 +1,10 @@
 from http.client import HTTPResponse
 
 from django.shortcuts import render, get_object_or_404, redirect
-from config.settings import MEDIA_ROOT
+
 # Create your views here.
+from django.template import loader
+
 from member.models import User
 from post.models import Post
 
@@ -26,12 +28,32 @@ def post_list(request):
 
 
 def post_detail(request, post_pk):
-    # post_pk에 해당하는 Post 객체를 리턴 / 보여줌
+    # Model(DB)에서 post_pk에 해당하는 Post객체를 가져와 변수에 할당
+    # ModelManager의 get 메서드를 통해 단 한 개의 객체만 가져온다.
     post = get_object_or_404(Post, pk=post_pk)
+
+    # request에 대해 response를 돌려줄 때는 HTTPResponse나 render함수 사용
+    # template을 사용할 경우 render함수를 사용한다
+    # render함수는
+    #   django.template.loader.get_template함수와
+    #   django.http.HTTPResponse함수를 축약해 놓은 shortcut
+
+    # ! 이 뷰에서는 render를 사용하지 않고 전체 과정(loader, HttpResponse)을 기술
+    # Django가 템플릿을 검색할 수 있는 모든 디렉토리를 순회하며
+    # 인자로 주어진 문자열 값과 일치하는 템플릿이 있는지 확인후
+    # 템플릿을 template에 리턴 (django.template.backends.django.Template클래스형 객체)
+    template = loader.get_template('post/post_detail.html')
+    # dict형 변수 context의 'post'키에 post(Post객체) 할당
     context = {
+        # context로 전달될 dictionary의 key값이 템플릿에서 사용가능한 변수명이 된다.
         'post': post,
     }
-    return render(request, 'post/post_detail.html', context=context)
+    # template에 인자로 주어진 context, request를 render함수로 string을 받아온다.
+    rendered_string = render(request=request, context=context)
+    return HTTPResponse('rendered_string')
+
+    # render를 사용하면 위의 과정을 함축하여 사용함과 동시에 post_detail.html으로 연결해준다.
+    # return render(request, 'post/post_detail.html', context=context)
 
 
 def post_create(request):
